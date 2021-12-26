27cd4f87b1f5db3e65ea91d138195f1089980982
utkubuzol <utkuebuzol@gmail.com>
2021-12-26 13:02:03 +0300

    bmi_calculator
    #100DaysOfCoding Day2 exercise

---
+++ bmi_calculator.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ") #input foncsions
+weight = input("enter your weight in kg: ")  #are STR
+# 🚨 Don't change the code above 👆
+# BMI CALCULATOR
+#Write your code below this line 👇
+
+bmi = int(weight) / float(height)** 2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+
+
+
+
+
+
